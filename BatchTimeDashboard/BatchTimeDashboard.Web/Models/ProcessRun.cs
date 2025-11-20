7212d1591aa071b6bb84d49b41ae4e97343cdae8
Aaron Finazzo <finazzocodes@gmail.com>
2025-11-20 13:58:32 -0500

    feat: add ProcessRun model

---
+++ BatchTimeDashboard/BatchTimeDashboard.Web/Models/ProcessRun.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BatchTimeDashboard.Web.Models
+{
+    public class ProcessRun
+    {
+        public string ProductCode { get; set; } = "";
+        public string BatchId { get; set; } = "";
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        // Auto-calculated property for duration
+        public double DurationMinutes => (EndTime - StartTime).TotalMinutes;
+    }
+}
--- BatchTimeDashboard/BatchTimeDashboard.Web/obj/Debug/net10.0/BatchTimeDashboard.Web.AssemblyInfo.cs
+++ BatchTimeDashboard/BatchTimeDashboard.Web/obj/Debug/net10.0/BatchTimeDashboard.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BatchTimeDashboard.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18eae6074a573d9e4dee397e5fd1c9012aec1970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab75cda3802acf61316c808c2d0083af62481da")]
 [assembly: System.Reflection.AssemblyProductAttribute("BatchTimeDashboard.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BatchTimeDashboard.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
